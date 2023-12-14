b50312e41150a7d15311849e1058dc442d12daef
oliver.kelly <foolishhim@gmail.com>
2023-12-14 11:24:22 +0000

    formatting

--- bash/rsync-notifyer.sh
+++ bash/rsync-notifyer.sh
@@ -9,7 +9,6 @@ WEBHOOK_URL=$WEBHOOK_URL
 
 # Run rsync command from cron using 00 4 * * * /bin/bash /root/rsync-notifyer.sh
 if rsync -avx  /home/pi/nfsdocker-volumes /glusterfs/data/nfsdocker-volumes ; then
-
     TITLE="✅ rsync completed on '$HOSTNAME' ✅"
     COLOUR=65280
     DATETIME=$(/usr/bin/date)
@@ -20,10 +19,9 @@ if rsync -avx  /home/pi/nfsdocker-volumes /glusterfs/data/nfsdocker-volumes ; th
         --arg message "$MESSAGE" \
         --arg hostname "$HOSTNAME" \
         '{"embeds":[{"title": $title , "color": ($colour | tonumber), "fields": [{"name": "host","value": $hostname },{"name": "Completed at","value": $message,}]}]}')
-    
+
     curl -X POST -H "Content-Type:application/json" --data "$discord_message" "$WEBHOOK_URL"
 else
-
     TITLE="❌ rsync failed on '$HOSTNAME' ❌"
     COLOUR=16711680
     DATETIME=$(/usr/bin/date)
@@ -37,4 +35,3 @@ else
 
     curl -X POST -H "Content-Type:application/json" --data "$discord_message" "$WEBHOOK_URL"
 fi
-
