d738d878bc61c6f09b8cc8c8e622f5183ff44dd5
oliver.kelly <foolishhim@gmail.com>
2023-12-14 11:09:53 +0000

    added prettier JSON

--- bash/rsync-notifyer.sh
+++ bash/rsync-notifyer.sh
@@ -10,10 +10,31 @@ WEBHOOK_URL=$WEBHOOK_URL
 # Run rsync command from cron using 00 4 * * * /bin/bash /root/rsync-notifyer.sh
 if rsync -avx  /home/pi/nfsdocker-volumes /glusterfs/data/nfsdocker-volumes ; then
 
-    discord_message="rsync completed successfully on $HOSTNAME "
-    curl -X POST -H "Content-Type:application/json" --data '{"content":"'" $discord_message "'"}' $WEBHOOK_URL
+    TITLE="✅ rsync completed on '$HOSTNAME' ✅"
+    COLOUR=65280
+    DATETIME=$(/usr/bin/date)
+    MESSAGE="'$DATETIME'"
+    discord_message=$( jq -n \
+        --arg title "$TITLE" \
+        --arg colour "$COLOUR" \
+        --arg message "$MESSAGE" \
+        --arg hostname "$HOSTNAME" \
+        '{"embeds":[{"title": $title , "color": ($colour | tonumber), "fields": [{"name": "host","value": $hostname },{"name": "Completed at","value": $message,}]}]}')
+    
+    curl -X POST -H "Content-Type:application/json" --data "$discord_message" "$WEBHOOK_URL"
 else
-    discord_message="rsync failed with exit code $?. on $HOSTNAME"
-    curl -X POST -H "Content-Type:application/json" --data '{"content":"'" $discord_message "'"}' $WEBHOOK_URL
+
+    TITLE="❌ rsync failed on '$HOSTNAME' ❌"
+    COLOUR=16711680
+    DATETIME=$(/usr/bin/date)
+    MESSAGE="'$DATETIME'"
+    discord_message=$( jq -n \
+        --arg title "$TITLE" \
+        --arg colour "$COLOUR" \
+        --arg message "$MESSAGE" \
+        --arg hostname "$HOSTNAME" \
+        '{"embeds":[{"title": $title , "color": ($colour | tonumber), "fields": [{"name": "host","value": $hostname },{"name": "Failed at","value": $message,}]}]}')
+
+    curl -X POST -H "Content-Type:application/json" --data "$discord_message" "$WEBHOOK_URL"
 fi
 
